54bd17b1aff8bb32c094a057ed06f9ee219de430
abi <abimael.monarrez58@gmail.com>
2022-03-26 21:57:34 -0700

    create route to DELETE a friend from user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,13 +5,14 @@ const {
     createUser,
     updateUser,
     deleteUser,
-    addFriend
+    addFriend,
+    removeFriend
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-router.route('/:userId/friends/:friendId').post(addFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
