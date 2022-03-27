8f9aca2cfb6d7d69a5ac42457513d27275bd5dc1
abi <abimael.monarrez58@gmail.com>
2022-03-26 19:30:26 -0700

    create routes to GET all users and POST a new user

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const { User } = require('../models/index');
+
+module.exports = {
+    getUsers(req, res) {
+        User.find()
+            .then((users) => {
+                return res.json(users);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    }
+}
